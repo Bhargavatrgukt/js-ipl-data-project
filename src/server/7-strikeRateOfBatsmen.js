bbaef1e631c40c020aed1a77235867a0e7a23e5d
Bhargav <bhargavatrgukt@gmail.com>
2024-11-13 11:26:47 +0530

    stike-rate-of-batsmen-problem-solved

--- src/server/7-strikeRateOfBatsmen.js
+++ src/server/7-strikeRateOfBatsmen.js
@@ -11,10 +11,63 @@ const deliveriesData=dataFormatChange(filePath);
 const matchesData=dataFormatChange(matchFilePath);
 
 
-const outputPath1=path.join(__dirname,"../output/matches.json");
 
-fs.writeFileSync(outputPath1,JSON.stringify(matchesData,null,2));
+// Strike rate of a batsman for each season
 
-const outputPath2=path.join(__dirname,"../output/deliveries.json");
+//first-step is to filter the ids'by season
 
-fs.writeFileSync(outputPath2,JSON.stringify(deliveriesData,null,2));
+let years={}
+
+for(let match of matchesData){
+    if(!years[match["season"]]){
+        years[match["season"]]=[];
+    }
+    years[match["season"]].push(match["id"])
+}
+
+//Step1 completed to extract id's from the matchesData with respective to year
+
+
+
+//Step2 ectracting bat'sMen stats  with respective to year
+let batsmenStats={}
+
+for (let eachYear in years) {
+    let eachYearDeliveries = deliveriesData.filter(delivery => years[eachYear].includes(delivery["match_id"]));
+    
+    if (!batsmenStats[eachYear]) {
+        batsmenStats[eachYear] = {};
+    }
+  
+    for(let eachDelivery of eachYearDeliveries){
+        let batsman=eachDelivery["batsman"]
+        let batsman_runs=parseInt(eachDelivery["batsman_runs"])
+        if(!batsmenStats[eachYear][batsman]){
+            batsmenStats[eachYear][batsman]={runs_played:0,balls_played:0}
+        }
+        batsmenStats[eachYear][batsman]["runs_played"]+=batsman_runs
+        batsmenStats[eachYear][batsman]["balls_played"]+=1
+    } 
+}
+
+
+
+//calculating StrikeRate  --->Final Step
+for(let eachYear in batsmenStats){
+    for(let player in batsmenStats[eachYear]){
+        let runsPlayed = batsmenStats[eachYear][player].runs_played;
+        let ballsPlayed = batsmenStats[eachYear][player].balls_played
+        let strike_rate=((runsPlayed/ballsPlayed)*100).toFixed(2)
+        batsmenStats[eachYear][player].strike_rate=strike_rate;
+    }
+}
+
+
+
+const outputPath1=path.join(__dirname,"../output/7-strikeRateOfBatsmen.json");
+
+fs.writeFileSync(outputPath1,JSON.stringify(batsmenStats,null,2));
+
+
+
+
